51c7bbbab563102140dc94c7311f97b8436db7fb
Ishan Deep <83365201+ishandeep48@users.noreply.github.com>
2026-09-08 16:50:00 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for (const auto& edge : prerequisites) {
+            int from = edge[1];
+            int to = edge[0];
+            adj[from].push_back(to);
+        }
+        vector<int> ingress(numCourses);
+        for (const auto& i : adj) {
+            for (int j : i)
+                ingress[j]++;
+        }
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (ingress[i] == 0)
+                q.push(i);
+        }
+        vector<int> ans;
+        while (!q.empty()) {
+            int temp = q.front();
+            q.pop();
+            ans.push_back(temp);
+            for (int& i : adj[temp]) {
+                ingress[i]--;
+                if (ingress[i] == 0)
+                    q.push(i);
+            }
+        }
+        for (int& i : ingress) {
+            if (i > 0) {
+                return {};
+            }
+        }
+        return ans;
+    }
+};
